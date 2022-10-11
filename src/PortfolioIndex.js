27484048d3e388a7a4994d0dfac274b2ed381789
hannah <htwiggsmith@gmail.com>
2022-10-10 17:01:18 -0700

    more deployment tests

--- src/PortfolioIndex.js
+++ src/PortfolioIndex.js
@@ -1,6 +1,6 @@
-import { LitElement, html, css } from 'lit';
-import { styleMap } from 'lit/directives/style-map.js';
-import { themes } from './themes';
+import { LitElement, html, css } from "lit";
+import { styleMap } from "lit/directives/style-map.js";
+import { themes } from "./themes";
 
 export class PortfolioIndex extends LitElement {
   static properties = {
@@ -19,14 +19,21 @@ export class PortfolioIndex extends LitElement {
   }
   constructor() {
     super();
-    this.theme = 'nord';
+    this.theme = "nord";
   }
 
   render() {
     const theme = themes[this.theme];
     return html`
       <main style=${styleMap(theme)}>
-        <h1>hannah</h1>
+      <h1>hannah twigg-smith</h1>
+      <img src="public/images/grid_painting.jpg" class="portrait" width=200></a>
+        <p class="bio">
+          I'm Hannah! I'm a PhD student in Human Centered Design and Engineering at the University of Washington. I work in
+          Nadya Peek's lab, Machine Agency. I'm work on tools for personal fabrication and automation, specifically the
+          software that allows us to tell machines what we want them to do.
+          <br>
+        </p>
       </main>
     `;
   }
